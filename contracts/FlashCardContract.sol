1b18a158a248ef51d5bc89864b3d53daa9a58faa
raiyanmook27 <raiyan.mook27@gmail.com>
2022-10-02 02:43:07 +0000

    Gas saving

--- contracts/FlashCardContract.sol
+++ contracts/FlashCardContract.sol
@@ -18,13 +18,14 @@ contract FlashCardContract {
      */
 
     function createNewCard(uint256 _amount, bytes32 _keyHash) public payable returns (uint256 cardId) {
+	  uint256 _cardCount =  cardCount; // gas saving
         require(_amount == msg.value, "Invalid amount");
-        uint256 id = cardCount + 1;
+        uint256 id = _cardCount + 1;
         cardIds[id] = msg.sender;
         cardAmounts[id] = _amount;
         cardKeys[id] = _keyHash;
-        cardCount++;
-        return cardCount;
+        _cardCount++;
+        return _cardCount;
     }
 
     /**
@@ -33,7 +34,8 @@ contract FlashCardContract {
      * @param _password is the non-encrypted version of key to redeem the card
      */
     function redeemCard(uint256 _cardId, string memory _password) public payable {
-        require(_cardId >= cardCount, "Invalid Card Id");
+ 	  uint256 _cardCount =  cardCount; // gas saving
+        require(_cardId >= _cardCount, "Invalid Card Id");
         bytes32 key = cardKeys[_cardId];
         require(cardAmounts[_cardId] > 0, "Insufficient card balance");
         bytes32 passKey = keccak256(abi.encodePacked(_password));
@@ -52,7 +54,8 @@ contract FlashCardContract {
      * @param _amount to be redeemed
      */
     function redeemCardPartial(uint256 _cardId, string memory _password, bytes32 _newKeyHash, uint256 _amount) public payable {
-        require(_cardId >= cardCount, "Invalid Card Id");
+	  uint256 _cardCount =  cardCount; // gas saving
+        require(_cardId >= _cardCount, "Invalid Card Id");
         bytes32 key = cardKeys[_cardId];
         require(cardAmounts[_cardId] > 0, "Insufficient card balance");
         bytes32 passKey = keccak256(abi.encodePacked(_password));
@@ -74,7 +77,8 @@ contract FlashCardContract {
      * require(msg.sender == cardIds[_cardId], "Unauthorized: Owner only");
      */
     function redeemCardTo(address to, uint256 _cardId, string memory _password) public payable {
-        require(_cardId >= cardCount, "Invalid Card Id");
+	  uint256 _cardCount =  cardCount; // gas saving
+        require(_cardId >= _cardCount, "Invalid Card Id");
         bytes32 key = cardKeys[_cardId];
         require(cardAmounts[_cardId] > 0, "Insufficient card balance");
         bytes32 passKey = keccak256(abi.encodePacked(_password));
@@ -95,7 +99,8 @@ contract FlashCardContract {
      * @param _amount to be redeemed
      */
     function redeemCardPartialTo(address to, uint256 _cardId, string memory _password, bytes32 _newKeyHash, uint256 _amount) public payable {
-        require(_cardId >= cardCount, "Invalid Card Id");
+	  uint256 _cardCount =  cardCount; // gas saving
+        require(_cardId >= _cardCount, "Invalid Card Id");
         bytes32 key = cardKeys[_cardId];
         require(cardAmounts[_cardId] > 0, "Insufficient card balance");
         bytes32 passKey = keccak256(abi.encodePacked(_password));
@@ -111,11 +116,13 @@ contract FlashCardContract {
 
     // get functions
     function checkCardBalance(uint256 _cardId) public view returns (uint256 _amount) {
-        require(_cardId >= cardCount, "Invalid Card Id");
+	  uint256 _cardCount =  cardCount; // gas saving
+        require(_cardId >= _cardCount, "Invalid Card Id");
         return cardAmounts[_cardId];
     }
     function isCardOwner(address owner, uint256 _cardId) public view returns (bool) {
-        require(_cardId >= cardCount, "Invalid Card Id");
+	  uint256 _cardCount =  cardCount; // gas saving
+        require(_cardId >= _cardCount, "Invalid Card Id");
         if (cardIds[_cardId] == to) {
             return true;
         } else {
